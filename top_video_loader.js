c1833c0fa2473291b9e917597e9c57810f8f94cd
seanjwang <sean.j.wang@gmail.com>
2021-09-21 14:57:56 -0400

    load top video individually

---
+++ top_video_loader.js
@@ -0,0 +1,18 @@
+var top_video_dir = "top_videos/"
+var top_video_list = ["top_video_1.mp4","top_video_2.mp4","top_video_3.mp4"];
+var top_video_background_list = ["top_video_1.png","top_video_2.png","top_video_3.png"];
+var top_video_index = 0;
+
+top_video=document.getElementById("top_video");
+top_video.onloadstart = loadNextTopVideo();
+
+function loadNextTopVideo(){
+	console.log("loading next top video");
+	top_video.setAttribute("src",top_video_dir+top_video_list[top_video_index]);
+	if (top_video_index < top_video_list.length -1){
+		top_video_index++;
+	}
+	else{
+		top_video_index = 0;
+	}
+}
