bd61f634026163fa06029f05967c6e26e259da86
seanjwang <sean.j.wang@gmail.com>
2021-09-21 14:59:29 -0400

    added picture preview

--- top_video_loader.js
+++ top_video_loader.js
@@ -7,6 +7,7 @@ top_video=document.getElementById("top_video");
 top_video.onloadstart = loadNextTopVideo();
 
 function loadNextTopVideo(){
+	top_video.style.backgroundImage = "url("+top_video_dir+top_video_background_list[top_video_index]+")"
 	console.log("loading next top video");
 	top_video.setAttribute("src",top_video_dir+top_video_list[top_video_index]);
 	if (top_video_index < top_video_list.length -1){
